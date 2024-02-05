b26b52c9fc11cf2927b26e9130dc1744af6fdbd6
Gleb Goloborodko <glbgoloborodko@yandex.ru>
2024-02-05 23:45:44 +0300

    зарегистрирована модель поста для админки

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from blog.models import Post
+
+
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'time_create', 'time_update', 'is_published']
+    list_display_links = ['title']
+    search_fields = ['title', 'content']
+    list_filter = ['is_published', 'time_update']
+    prepopulated_fields = {'slug': ('title',)}
+    list_editable = ['is_published']
